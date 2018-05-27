c9bb0e7ed29aa2c23cd77a20f25397e3427e41b6
Johny Klein <johnyklein@hotmail.com>
2018-05-27 11:29:16 -0300

    Criado entity class

---
+++ js/entity.js
@@ -0,0 +1,47 @@
+var Size = class Size {
+    constructor(size="", image="", price="") {
+        this.size = "";
+        this.image = image;
+        this.price = price;
+    }
+
+    getClassName() {
+        return Size.name;
+    }
+
+    getLiteralName(){
+        return "Tamanhos";
+    }
+}
+
+var Flavor = class Flavor {
+    constructor(flavor="", image="", price="") {
+        this.flavor = "";
+        this.image = image;
+        this.price = price;
+    }
+
+    getClassName() {
+        return Flavor.name;
+    }
+
+    getLiteralName(){
+        return "Sabores";
+    }
+}
+
+var Drink = class Drink {
+    constructor(drink="", image="", price="") {
+        this.drink = "";
+        this.image = image;
+        this.price = price;
+    }
+
+    getClassName() {
+        return Drink.name;
+    }
+
+    getLiteralName(){
+        return "Bebidas";
+    }
+}
