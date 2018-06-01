86181b33bf45aa528f0a326e707329e678525c29
Johny Klein <johnyklein@hotmail.com>
2018-05-31 23:02:31 -0300

    Adicionado imagens nos arrays

--- js/setData.js
+++ js/setData.js
@@ -5,17 +5,17 @@ var size3 = new Size("Pequeno (20cm)", "teste.jpg", 20.99);
 sizes = [size1, size2, size3];
 
 var flavors = [];
-var flavor1 = new Flavor("Quatro queijos", "teste.jpg", 40.99);    
-var flavor2 = new Flavor("Calabresa", "teste.jpg", 29.99);    
-var flavor3 = new Flavor("Tomate seco", "teste.jpg", 29.99);    
-var flavor4 = new Flavor("Banana nevada", "teste.jpg", 29.99);    
-var flavor5 = new Flavor("Prestígio", "teste.jpg", 29.99);    
+var flavor1 = new Flavor("Quatro queijos", "pizza-quatro_queijos.jpg", 40.99);    
+var flavor2 = new Flavor("Calabresa", "pizza-calabresa.jpg", 30.99);    
+var flavor3 = new Flavor("Tomate seco", "pizza-tomate_seco.jpg", 19.99);    
+var flavor4 = new Flavor("Banana nevada", "pizza-banana_nevada.jpg", 59.99);    
+var flavor5 = new Flavor("Prestígio", "pizza-prestigio.jpg", 79.99);    
 flavors = [flavor1, flavor2, flavor3, flavor4, flavor5];
 
 var drinks = [];
-var drink1 = new Drink("Suco de laranja", "teste.jpg", 4.99);   
-var drink2 = new Drink("Suco de morango", "teste.jpg", 4.99);   
-var drink3 = new Drink("Coca-cola", "teste.jpg", 9.99);   
-var drink4 = new Drink("Fanta uva", "teste.jpg", 7.99);   
-var drink5 = new Drink("Caipirinha", "teste.jpg", 5.99);   
+var drink1 = new Drink("Suco de laranja", "suco-laranja.jpg", 4.99);   
+var drink2 = new Drink("Suco de morango", "suco-morango.jpg", 4.99);   
+var drink3 = new Drink("Coca-cola", "coca-cola.jpg", 9.99);   
+var drink4 = new Drink("Fanta uva", "fanta-uva.jpg", 7.99);   
+var drink5 = new Drink("Caipirinha", "caipirinha.jpg", 5.99);   
 drinks = [drink1, drink2, drink3, drink4, drink5];
